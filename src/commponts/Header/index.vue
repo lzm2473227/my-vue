1f5eac32c228aaf88805d42f2a75faa20f5b1d6a
liu <2806644613@qq.com>
2020-11-27 19:23:31 +0800

    feat：页面结构

--- src/commponts/Header/index.vue
+++ src/commponts/Header/index.vue
@@ -1,15 +1,110 @@
 <template>
-  <div>
-      ...Header
-  </div>
+  <header class="header">
+    <div class="header-top">
+      <div class="header-container">
+        <div class="login-list">
+          <p>尚品汇欢迎您！</p>
+          <p>
+            <span>请</span>
+            <a href="#">登录</a>
+            <a href="#" class="register">免费注册</a>
+          </p>
+        </div>
+        <div class="type-list">
+          <a href="#">我的订单</a>
+          <a href="#">我的购物车</a>
+          <a href="#">我的尚品汇</a>
+          <a href="#">尚品汇会员</a>
+          <a href="#">企业采购</a>
+          <a href="#">关注尚品汇</a>
+          <a href="#">合作招商</a>
+          <a href="#">商家后台</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="header-bottom">
+      <h1 class="logo">
+        <a href="#">
+          <img src="./images/logo.png" alt="" />
+        </a>
+      </h1>
+      <div class="header-search">
+        <form action="" class="search-form">
+          <input
+            type="text"
+            id="autocomplete"
+            class="sui-btn btn-xlarge btn-danger"
+          />
+          <button class="sui-btn btn-xlarge btn-danger">搜索</button>
+        </form>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-}
+  name: "Header",
+};
 </script>
 
 <style lang="less" scoped>
+.header-top {
+  background-color: #eaeaea;
+  height: 30px;
+  line-height: 30px;
+}
+.header-container {
+  display: flex;
+  width: 1200px;
+  margin: 0 auto;
+  justify-content: space-between;
+}
+.login-list {
+  display: flex;
+}
 
+.login-list p {
+  margin-right: 10px;
+}
+.type-list a + a {
+  border-left: 1px solid #b3aeae;
+}
+.type-list a {
+  padding: 0 10px;
+}
+.register {
+  border-left: 1px solid #b3aeae;
+  padding: 0 5px;
+  margin-left: 5px;
+}
+
+.header-bottom {
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+h1 {
+  font-size: 18px;
+}
+.header-search {
+  margin-top: 35px;
+  input {
+    box-sizing: border-box;
+    width: 490px;
+    height: 32px;
+    padding: 0px 4px;
+    border: 2px solid #ea4a36;
+  }
+  button {
+    height: 32px;
+    width: 68px;
+    background-color: #ea4a36;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+  }
+}
 </style>
