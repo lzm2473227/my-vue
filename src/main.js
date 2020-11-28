5676a1a18cfb54294101f0ebc4299b955128924f
liu <2806644613@qq.com>
2020-11-28 13:32:03 +0800

    feat:重写push和replace

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from "./router";
 import './styles/reset.css'
 
 Vue.config.productionTip = false;
-
+console.log(router);
 /* eslint-disable no-new */
 new Vue({
   render: (h) => h(App),
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,55 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from '../views/Home'
-import Login from '../views/Login'
-import Register from '../views/Register'
-import Search from '../views/Search'
+import Home from "../views/Home";
+import Login from "../views/Login";
+import Register from "../views/Register";
+import Search from "../views/Search";
+
+const push = VueRouter.prototype.push;
+const replace = VueRouter.prototype.replace;
+
+VueRouter.prototype.push = function(location,onComplete,onAbout) {
+    if(onComplete && onAbout){
+        return push.call(this,location,onComplete,onAbout)
+    }
+    return push.call(this,location,onComplete,()=>{})
+};
+VueRouter.prototype.replace = function(location,onComplete,onAbout) {
+    if(onComplete && onAbout){
+        return replace.call(this,location,onComplete,onAbout)
+    }
+    return replace.call(this,location,onComplete,()=>{})
+};
+
+// console.log(VueRouter.prototype);
+
 
 
 Vue.use(VueRouter);
 
-
-
 export default new VueRouter({
-    routes:[
-        {
-            path:'/',
-            component:Home
-        },
-        {
-            path:'/login',
-            component:Login,
-            meta:{
-                isfooterShow:true
-            }
-        },
-        {
-            path:'/register',
-            component:Register,
-            meta:{
-                isfooterShow:true
-            }
-        },
-        {
-            path:'/search/:searchText?',
-            component:Search
-        }
-    ]
-})
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/login",
+      component: Login,
+      meta: {
+        isfooterShow: true,
+      },
+    },
+    {
+      path: "/register",
+      component: Register,
+      meta: {
+        isfooterShow: true,
+      },
+    },
+    {
+      path: "/search/:searchText?",
+      component: Search,
+    },
+  ],
+});
