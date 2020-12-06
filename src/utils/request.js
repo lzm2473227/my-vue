734ccb51ee73f584878e1bf43afee947f4ad4df9
liu <2806644613@qq.com>
2020-12-06 18:12:50 +0800

    feat：优化

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { Message } from "element-ui"; //错误按钮提示
-import getUserTemId from "@utils/getUserTempld";
+import getUserTempId from "@utils/getUserTempld";
 import NProgress from "nprogress"; //进度条的包
 import "nprogress/nprogress.css";
 
-const userTemId = getUserTemId();
+const userTempId = getUserTempId();
 
 const instance = axios.create({
   baseURL: "/api",
@@ -15,7 +15,7 @@ const instance = axios.create({
 instance.interceptors.request.use((config) => {
   // 开始进度条   请求拦截器触发的时候开始进度条
   NProgress.start();
-  config.headers.userTemId = userTemId;
+  config.headers.userTempId = userTempId;
 
   return config;
 });
